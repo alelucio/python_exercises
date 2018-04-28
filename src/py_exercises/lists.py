ba1ad0980b03671885d23453cbc87bcb0e9e9a95
Alessandro Luciani <ale.is.lucio@gmail.com>
2018-04-28 19:11:14 +0200

    ho aggiornato questo modulo con le funzioni di aletzee perchè qua posso fare i test, mi manda in bestia la funzione large_straight

--- src/py_exercises/lists.py
+++ src/py_exercises/lists.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from functools import reduce
+
 from py_exercises.recursion import *
 
 
@@ -16,3 +18,85 @@ def my_but_last(arr):
     """Find the last but one element of a list"""
 
     return arr[-2]
+
+
+# inverti una lista #
+
+def reverse_a_list(arr):
+    return arr[::-1]
+
+
+# palindromo o no#
+
+def is_palindrome(arr):
+    if arr[::1] == arr[::-1]:
+        return True
+    else:
+        return False
+
+
+def is_palindrome_1(arr):
+    return arr[::1] == arr[::-1]
+
+
+def is_palindrome_2(arr):
+    return arr == reverse_a_list(arr)
+
+
+# togli i duplicati #
+
+def destroy_duplicates(arr):
+    def compatta(acc, nextt):
+        if nextt == acc[-1]:
+            return acc
+        else:
+            return acc + nextt
+
+    return reduce(compatta, arr)
+
+
+def destroy_duplicates_1(arr):
+    def compatta(acc, nextt):
+        return acc if nextt == acc[-1] else acc + nextt
+
+    return reduce(compatta, arr)
+
+
+def destroy_duplicates_2(arr):
+    return reduce(lambda acc, nextt: acc if nextt == acc[-1] else acc + nextt, arr)
+
+
+# funzioni aletzee #
+
+def select_number(arr):
+    return list(filter(lambda x: x == 1, arr))
+
+
+def select_number_1(arr):
+    lista_numeri = list(filter(lambda x: x == 1, arr))
+    return lista_numeri
+    print(lista_numeri)
+
+
+def sum_number(arr):
+    return reduce(lambda x, y: x + y, select_number(arr))  # fatto ma non capito benissimo#
+
+
+def three_of_kind_ones(arr):
+    if sum_number(arr) == 3:
+        return sum(arr)
+    else:
+        return None
+
+
+def three_of_kind_ones_1(arr):
+    return sum(arr) if sum_number(arr) == 3 else None
+
+
+def four_of_kind_ones(arr):
+    return sum(arr) if sum_number(arr) == 4 else None
+
+
+def large_straight(arr):
+    scala = arr.sort()
+    return 40 if scala == [1, 2, 3, 4, 5]  else None
--- tests/test_lists.py
+++ tests/test_lists.py
@@ -25,3 +25,68 @@ def test_my_but_last():
     assert my_but_last([1, 2, 3, 4]) == 3
     with pytest.raises(IndexError):
         my_but_last([1])
+
+
+def test_reverse_a_list():
+    assert reverse_a_list(["c", "i", "a", "o"]) == ["o", "a", "i", "c"]
+    assert reverse_a_list("ciao") == "oaic"
+    assert reverse_a_list([]) == []
+    assert reverse_a_list("ciao") != "miao"
+
+
+def test_is_palindrome():
+    assert is_palindrome("osso")
+    assert not is_palindrome("pizza")
+    assert is_palindrome([])
+
+
+def test_destroy_duplicates():
+    assert destroy_duplicates("babbbo") == "babo"
+    assert destroy_duplicates("babbbo") != "babbo"
+
+
+def test_destroy_duplicates_2():
+    assert destroy_duplicates_2("babbbo") == "babo"
+    assert destroy_duplicates_2("babbbo") != "babbo"
+
+
+# aletzee test #
+
+def test_select_number():
+    assert select_number([1, 4, 5, 1, 3]) == [1, 1]
+    assert select_number([]) == []
+    assert select_number([1, 4, 5, 2, 3]) != [1, 1]
+
+
+def test_select_number_1():
+    assert select_number_1([1, 4, 5, 1, 3]) == [1, 1]
+    assert select_number([]) == []
+    assert select_number([1, 4, 5, 2, 3]) != [1, 1]
+
+
+def test_sum_number():
+    assert sum_number([1, 4, 5, 1, 3]) == 2
+    # assert sum_number([]) == []#
+    assert sum_number([1, 4, 5, 2, 3]) != 2
+
+
+def test_three_of_kind_ones():
+    assert three_of_kind_ones([1, 1, 1, 4, 5]) == 12
+    assert three_of_kind_ones([1, 1, 1, 1, 6]) != 12
+    # assert three_of_kind_ones([2, 2, 4, 5, 5]) != 12#
+
+
+def test_three_of_kind_ones_1():
+    assert three_of_kind_ones_1([1, 1, 1, 4, 5]) == 12
+    assert three_of_kind_ones([1, 1, 4, 3, 6]) != 12
+
+
+def test_four_of_kind_ones():
+    assert four_of_kind_ones([1, 1, 1, 1, 6]) == 10
+    assert four_of_kind_ones([1, 1, 5, 1, 6]) != 10
+
+
+def test_large_straight():
+    assert large_straight([1, 2, 3, 4, 5]) == 40
+    assert large_straight([5, 2, 2, 4, 5]) != 40
+
